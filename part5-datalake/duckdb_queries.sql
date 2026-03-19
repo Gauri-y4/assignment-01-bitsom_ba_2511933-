79ba656bb710a15ddb568e5c2a993332cafbd2af
Gauri-y4 <gaurisat07@gmail.com>
2026-03-19 12:25:57 +0530

    Add DuckDB cross-format queries

--- part5-datalake/duckdb_queries.sql
+++ part5-datalake/duckdb_queries.sql
@@ -1,9 +1,7 @@
 -- Q1: List all customers along with the total number of orders they have placed
 
-SELECT 
-    c.customer_id,
-    c.customer_name,
-    COUNT(o.order_id) AS total_orders
+SELECT c.customer_id,c.customer_name,
+COUNT(o.order_id) AS total_orders
 FROM read_csv_auto('datasets/customers.csv') c
 LEFT JOIN read_json_auto('datasets/orders.json') o
 ON c.customer_id = o.customer_id
@@ -11,9 +9,7 @@ GROUP BY c.customer_id, c.customer_name;
 
 -- Q2: Find the top 3 customers by total order value
 
-SELECT 
-    c.customer_name,
-    SUM(o.quantity * p.price) AS total_order_value
+SELECT c.customer_name,SUM(o.quantity * p.price) AS total_order_value
 FROM read_csv_auto('datasets/customers.csv') c
 JOIN read_json_auto('datasets/orders.json') o
 ON c.customer_id = o.customer_id
@@ -25,8 +21,7 @@ LIMIT 3;
 
 -- Q3: List all products purchased by customers from Bangalore
 
-SELECT DISTINCT
-    p.product_name
+SELECT DISTINCT p.product_name
 FROM read_csv_auto('datasets/customers.csv') c
 JOIN read_json_auto('datasets/orders.json') o
 ON c.customer_id = o.customer_id
@@ -36,11 +31,7 @@ WHERE c.city = 'Bangalore';
 
 -- Q4: Join all three files to show: customer name, order date, product name, and quantity
 
-SELECT
-    c.customer_name,
-    o.order_date,
-    p.product_name,
-    o.quantity
+SELECT c.customer_name,o.order_date,p.product_name, o.quantity
 FROM read_csv_auto('datasets/customers.csv') c
 JOIN read_json_auto('datasets/orders.json') o
 ON c.customer_id = o.customer_id
